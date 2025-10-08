61134124f6833e80e593c6de29e406d485b5f470
Pfyber <luka.colaric@gmail.com>
2025-10-08 07:54:50 +0000

    navodila

--- app.py
+++ app.py
@@ -23,4 +23,7 @@ app.run(debug=True, port=7777 )
 
 # napiši route /randomOseba
 # ki zgenerira random ime, priimek, starost in kraj rojstva
+# naslov in random številko naslova
 # sezname generiraj z AI
+# na koncu celoten HTML daj v AI 
+# AI promprtaj naj naredi lep CSS za html file
